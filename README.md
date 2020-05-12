0cefc99861bad7e70fdbb115c97c1b12ca85ed9c
Zhalev <zhalev>
2020-05-12 20:19:18 +0300

    Master commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on May 2, 2020, 2:01 pm.
---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@BloggerBlog/Page/index.html.twig');
+
+    }
+    public function aboutAction()
+    {
+        return $this->render('@BloggerBlog/Page/about.html.twig');
+    }
+    public function contactAction(Request $request)
+    {
+        $enquiry = new Enquiry();
+
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        if ($request->isMethod($request::METHOD_POST)) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('@BloggerBlog/Page/contactEmail.txt.twig', array('enquiry' => $enquiry)));
+
+
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('@BloggerBlog/Page/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+}
