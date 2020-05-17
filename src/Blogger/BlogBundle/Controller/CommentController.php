66d3ec1f805fcc30ba4502c029fe0a8a1d445eee
Zhalev <zhalev@bk.ru>
2020-05-17 21:08:45 +0300

    Comment form

---
+++ src/Blogger/BlogBundle/Controller/CommentController.php
@@ -0,0 +1,70 @@
+<?php
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blogger\BlogBundle\Entity\Comment;
+use Blogger\BlogBundle\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Comment controller.
+ */
+class CommentController extends Controller
+{
+    public function newAction($blog_id)
+    {
+        $blog = $this->getBlog($blog_id);
+
+        $comment = new Comment();
+        $comment->setBlog($blog);
+        $form   = $this->createForm(CommentType::class, $comment);
+
+        return $this->render('@BloggerBlog/Comment/form.html.twig', array(
+            'comment' => $comment,
+            'form'   => $form->createView()
+        ));
+    }
+
+    public function createAction(Request $request, $blog_id)
+    {
+        $blog = $this->getBlog($blog_id);
+
+        $comment  = new Comment();
+        $comment->setBlog($blog);
+        $form    = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()
+                ->getManager();
+            $em->persist($comment);
+            $em->flush();
+            // TODO: Persist the comment entity
+
+            return $this->redirect($this->generateUrl('BloggerBlogBundle_blog_show', array(
+                    'id' => $comment->getBlog()->getId())) .
+                '#comment-' . $comment->getId()
+            );
+        }
+
+        return $this->render('@BloggerBlog/Comment/create.html.twig', array(
+            'comment' => $comment,
+            'form'    => $form->createView()
+        ));
+    }
+
+    protected function getBlog($blog_id)
+    {
+        $em = $this->getDoctrine()
+            ->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($blog_id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $blog;
+    }
+
+}
---
+++ src/Blogger/BlogBundle/Form/CommentType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Blogger\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('user')
+            ->add('comment');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blogger\BlogBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'blogger_blogbundle_comment';
+    }
+
+
+}
